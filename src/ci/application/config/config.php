2a50159115d7fa18d8ba6b820ccd6e855c105c96
Xinyang <lixinyang@gmail.com>
2011-05-30 18:00:43 +0800

    push push

--- src/ci/application/config/config.php
+++ src/ci/application/config/config.php
@@ -368,8 +368,8 @@ define( "TBL_SNS_BINDING" , 'sns_bindings' );
 define( "WB_AKEY" , '3340614448' );
 define( "WB_SKEY" , '4a9e022e77cf7f40ed253b076854fbf0' );
 
-define( "QQ_APPID" , '139749' );
-define( "QQ_APPKEY" , '86a8afbc6456390cc5a655acf81d9990' );
+define( "QQ_APPID" , '207680' );
+define( "QQ_APPKEY" , '847ef39e25b9766dd48b764b7c302c97' );
 
 define( "RENREN_APPKEY" , '60ca3fd7dfeb48ae9527cf9becc8f961' );
 define( "RENREN_APPSECRET" , '21d55ece68ec43599ed91aa844c3f51e' );
